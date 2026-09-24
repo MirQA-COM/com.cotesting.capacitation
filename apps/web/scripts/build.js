9e76ce5f33e5da2a803c225abd9345319c68e629
skoutperiferia53 <skoutperiferia53@skandia.com.co>
2026-09-23 21:24:55 -0500

    Ajuste

---
+++ apps/web/scripts/build.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const srcDir = path.join(__dirname, '..', 'src');
+const distDir = path.join(__dirname, '..', 'dist');
+
+fs.rmSync(distDir, { recursive: true, force: true });
+fs.mkdirSync(distDir, { recursive: true });
+fs.cpSync(srcDir, distDir, { recursive: true });
+
+console.log('Build generado en apps/web/dist');
